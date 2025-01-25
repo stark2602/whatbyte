543d6c356b52aa8c9f6c30b884b97ccd25e398eb
Mayank Pratap Singh <98162499+stark2602@users.noreply.github.com>
2025-01-25 11:38:48 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@next/next/no-img-element": "off", 
+  },
+};
 
 export default eslintConfig;
